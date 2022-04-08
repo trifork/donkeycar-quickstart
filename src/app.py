6cfff5f61f12457304ec93461ab0fbaa53f09ba4
JonathanFL <j.lydemann@gmail.com>
2022-04-08 11:18:57 +0200

    It seems to work....

--- src/app.py
+++ src/app.py
@@ -4,23 +4,23 @@ import os, sys
 import zipfile
 import asyncio
 import shutil
+import secrets
+from classifier import DonkeyCarClassifier
+from logger import log
 from flask import Flask, render_template, request, send_file, redirect, url_for
 from werkzeug.utils import secure_filename
 from donkeycar.pipeline.training import train
 from donkeycar.management.base import load_config, CreateCar
 
-from constants import SetupConstants
 
-def log(content):
-   print(content, file=sys.stderr)
+
+def get_random_token():
+    """
+    Creates a cryptographically-secure, URL-safe string
+    """
+    return secrets.token_urlsafe(16)
 
 app = Flask(__name__)
-SetupConstants(app)
-
-ALLOWED_EXTENSIONS = set(['py', 'zip'])
-
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def upload():
@@ -28,62 +28,33 @@ def upload():
 
 @app.route('/train', methods = ['POST'])
 async def train_model():
-   tub_file_name = ''
-   # getting multiple files - both a config and a zip/tar something compressed
-   log(request.files)
 
-   files = request.files.getlist('file')
-   log(files)
+   c = CreateCar()
+   random_token = get_random_token()
+   unique_mycar_folder = os.path.join(app.root_path, 'mycar' + random_token)
+   log(unique_mycar_folder)
+   c.create_car(path=unique_mycar_folder)
+   data_folder = os.path.join(unique_mycar_folder, 'data')
+   models_folder = os.path.join(unique_mycar_folder, 'models')
+   model_file_name = 'mypilot.h5'
+
+   donkeyCarClassifier = DonkeyCarClassifier(unique_mycar_folder=unique_mycar_folder,model_path=models_folder,data_path=data_folder,model_file_name=model_file_name)
+
+   files = request.files.getlist('files[]')
    
    if not files:
       log("Not files")
       return redirect(url_for('upload'))
    
-   for file in files:
-      log('Filename: ' + file.filename)
-      if file and allowed_file(file.filename):
+   donkeyCarClassifier.set_files(files=files)
+   donkeyCarClassifier.train()
 
-         # TODO make uploaded files/folders names unique: get the files/folders
-         #      - save then with specific unique names on the server and train with the same unique names again later
-         # TODO show page loader when training
+   return send_file(donkeyCarClassifier.model_location, as_attachment=True, download_name=model_file_name)
 
-         # Convention: if not config.py, it is a zip of data
-         if '.py' not in file.filename:
-            tub_file_name = file.filename.replace('.zip', '')
-            log(tub_file_name)
-            with zipfile.ZipFile(file, 'r') as zip_ref:
-               zip_extraction_location = os.path.join(app.config['DATA_FOLDER'], tub_file_name)
-               log("zip_extraction_location: " + zip_extraction_location)
-               zip_ref.extractall(zip_extraction_location)
-         else:
-            file.save(os.path.join(app.config['MYCAR_FOLDER'], 'config.py'))
-
-   log('Train uploaded zip file')
-   config_file = os.path.join(app.config['MYCAR_FOLDER'], 'config.py')
-   log(config_file)
-   tub_file = os.path.join(app.config['DATA_FOLDER'], os.path.join(tub_file_name, tub_file_name))
-   log(tub_file)
-
-   cfg = load_config(config_path=config_file)
-
-   task = asyncio.create_task(train(cfg=cfg, tub_paths=tub_file, model=os.path.join(app.config['MODELS_FOLDER'], secure_filename(app.config['MODELS_FILE_NAME'])), model_type = None, transfer=None, comment=None))
-   # python base.py train --tub ../../mycar/data/tub_1_20-02-22_new/ --model ../../mycar/models/mypilot.h5 --config=../../mycar/config.py
-
-   # remove folders after training
-   shutil.rmtree(tub_file)
-
-   await task
-
-   return send_file(os.path.join(app.config['MODELS_FOLDER'], secure_filename(app.config['MODELS_FILE_NAME'])), as_attachment=True, download_name=app.config['MODELS_FILE_NAME'])
-
-@app.route('/basic-config', methods= ['GET'])
+@app.route('/myconfig', methods= ['GET'])
 def get_basic_config():
-   return send_file(os.path.join(app.config['MYCAR_FOLDER'], 'config.py'))
+   return send_file(os.path.join(app.root_path, 'myconfig.py'))
 
 if __name__ == "__main__":
-   c = CreateCar()
-   log(app.root_path + '/mycar')
-   c.create_car(path=app.root_path + '/mycar')
-
    port = int(os.environ.get('PORT', 5000))
    app.run(debug=True, host='0.0.0.0', port=port, use_reloader=False)
---
+++ src/classifier.py
@@ -0,0 +1,70 @@
+
+from tokenize import String
+from typing import List
+from xmlrpc.client import Boolean
+from logger import log
+import os
+import zipfile
+import shutil
+
+from donkeycar.pipeline.training import train
+from donkeycar.management.base import load_config
+
+from werkzeug.utils import secure_filename
+
+from cv2 import FileStorage
+
+
+class DonkeyCarClassifier:
+
+    def __allowed_file(self, filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in self.__ALLOWED_EXTENSIONS
+
+    def __init__(self, unique_mycar_folder, model_path:String, data_path:String, model_file_name:String) -> None:
+        self.model_path:String = model_path
+        self.__data_path:String = data_path
+        self.__model_file_name:String = model_file_name
+        self.__ALLOWED_EXTENSIONS = set(['py', 'zip'])
+        self.__unique_mycar_path = unique_mycar_folder
+        self.__tub_file_name = ''
+        self.__config_file_path = ''
+        self.__tub_file_path = ''
+
+    def set_files(self, files:List[FileStorage]) -> None:
+        self.files = files
+        for file in files:
+            log('Filename: ' + file.filename)
+            if file and self.__allowed_file(file.filename):
+                # Convention: if not .py, it is a zip of data
+                if '.py' not in file.filename:
+                    self.__tub_file_name = file.filename.replace('.zip', '')
+                    log(self.__tub_file_name)
+                    with zipfile.ZipFile(file, 'r') as zip_ref:
+                        zip_extraction_location = os.path.join(self.__data_path, self.__tub_file_name)
+                        log("zip_extraction_location: " + zip_extraction_location)
+                        zip_ref.extractall(zip_extraction_location)
+                else:
+                    file.save(os.path.join(self.__unique_mycar_path, 'myconfig.py'))
+
+        log("Getting config file")
+        self.__config_file_path = os.path.join(self.__unique_mycar_path, 'config.py')
+        log(self.__config_file_path)
+
+        log("Getting data folder location")
+        self.__tub_file_path = os.path.join(self.__data_path, os.path.join(self.__tub_file_name, self.__tub_file_name))
+        log(self.__tub_file_path)
+
+    def train(self) -> None:
+        cfg = load_config(config_path=self.__config_file_path)
+        log(cfg)
+        log('Train uploaded zip file')
+        # python base.py train --tub ../../mycar/data/tub_1_20-02-22_new/ --model ../../mycar/models/mypilot.h5 --config=../../mycar/config.py
+        history = train(cfg=cfg, tub_paths=self.__tub_file_path, model=os.path.join(self.model_path, secure_filename(self.__model_file_name)), model_type = None, transfer=None, comment=None)
+
+    @property
+    def model_location(self) -> String:
+        return os.path.join(self.model_path, secure_filename(self.__model_file_name))
+
+    def __del__(self) -> None:
+        # remove folders after training
+        shutil.rmtree(self.__unique_mycar_path)
